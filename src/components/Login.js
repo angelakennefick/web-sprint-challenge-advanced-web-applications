eb62e6f8ab09104adb2ead740fe1aac899f5d6b9
angelakennefick <78941432+angelakennefick@users.noreply.github.com>
2021-06-18 17:31:51 -0500

    Initial commit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,29 +1,58 @@
 import React, { useEffect } from "react";
+import { axiosWithAuth } from '../helpers/axiosWithAuth';
 
-const Login = () => {
+const login = e => {
+  e.preventDefault();
+  axiosWithAuth().post('login/endpoint', credentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.token);
+      this.props.history.push('/');
+    })
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  const error = "";
+  const error = "Username or Password not valid.";
   //replace with error state
+
+    const handleChange = e => {
+      setCredentials: {
+        ...credentials,
+        [e.target.name]: e.target.value,
+      }
+  }
 
   return (
     <div>
-      <h1>Welcome to the Bubble App!</h1>
-      <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
-      </div>
+      <form onSubmit={this.login}>
+        <h1>Welcome to the Bubble App!</h1>
+        <div data-testid="loginForm" className="login-form">
+          <input
+              type="text"
+              name="username"
+              value={credentials.username}
+              onChange={this.handleChange}
+              data-testid="username"
+            />
+            <input
+              type="password"
+              name="password"
+              value={credentials.password}
+              onChange={this.handleChange}
+              data-testid="password"
+            />
+          <button>Log in</button>
+        </div>
 
-      <p data-testid="errorMessage" className="error">{error}</p>
+        <p data-testid="errorMessage" className="error">{error}</p>
+      </form>
     </div>
   );
 };
 
 export default Login;
 
-//Task List:
-//1. Build a form containing a username and password field.
+
 //2. Add whatever state nessiary for form functioning.
-//3. MAKE SURE YOUR USERNAME AND PASSWORD INPUTS INCLUDE data-testid="username" and data-testid="password"
-//4. If either the username or password is not entered, display the following words with the p tag provided: Username or Password not valid.
+
+
 //5. If the username / password is equal to Lambda / i<3Lambd4, save that token to localStorage.
